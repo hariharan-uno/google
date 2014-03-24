9e2be1eea6b24c56c081191806ac56ee2bf82d5f
Hari haran <hariharan.uno@gmail.com>
2014-03-25 02:58:40 +0530

    Open link cross platform

--- google.go
+++ google.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"log"
 	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 
 	"github.com/codegangsta/cli"
 )
@@ -15,7 +19,21 @@ func main() {
 		if len(c.Args()) == 0 {
 			println("give a search query")
 		} else {
-			println("let me google", c.Args()[0])
+			s := strings.Join(c.Args(), "+")
+			println("let me google", s)
+			cmd := new(exec.Cmd)
+			switch runtime.GOOS {
+			case "linux":
+				cmd = exec.Command("xdg-open", "https://google.com/#q="+s)
+			case "windows":
+				cmd = exec.Command("start", "https://google.com/#q="+s)
+			case "darwin":
+				cmd = exec.Command("open", "https://google.com/#q="+s)
+			}
+			err := cmd.Start()
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 	app.Run(os.Args)

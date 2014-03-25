5bb03473c543e19420a4521c8109ca81e603cee8
Hari haran <hariharan.uno@gmail.com>
2014-03-25 11:59:13 +0530

    Update

--- google.go
+++ google.go
@@ -11,25 +11,31 @@ import (
 )
 
 func main() {
-
 	app := cli.NewApp()
 	app.Name = "google"
-	app.Usage = "Quick Search on google"
+	app.Usage = "Quick Search from terminal on google"
 	app.Action = func(c *cli.Context) {
 		if len(c.Args()) == 0 {
-			println("give a search query")
+			println("give a search query, e.g. \"google hello world\" ")
 		} else {
 			s := strings.Join(c.Args(), "+")
 			println("let me google", s)
+
 			cmd := new(exec.Cmd)
+
 			switch runtime.GOOS {
+
 			case "linux":
 				cmd = exec.Command("xdg-open", "https://google.com/#q="+s)
 			case "windows":
 				cmd = exec.Command("start", "https://google.com/#q="+s)
 			case "darwin":
 				cmd = exec.Command("open", "https://google.com/#q="+s)
+			default:
+				cmd = exec.Command("xdg-open", "https://google.com/#q="+s)
+				//TODO: Add support for more Operating Systems
 			}
+
 			err := cmd.Start()
 			if err != nil {
 				log.Fatal(err)

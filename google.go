1d596c1618385f1bdd57a4500668bf5d9b78dee7
Hari haran <hariharan.uno@gmail.com>
2014-03-31 04:40:48 +0530

    Remove external dependency

--- google.go
+++ google.go
@@ -1,46 +1,45 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"os"
 	"os/exec"
 	"runtime"
 	"strings"
-
-	"github.com/codegangsta/cli"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "google"
-	app.Usage = "Quick Search from terminal on google"
-	app.Action = func(c *cli.Context) {
-		if len(c.Args()) == 0 {
-			println("give a search query, e.g. \"google hello world\" ")
-		} else {
-			s := strings.Join(c.Args(), "+")
-			println("let me google", s)
 
-			cmd := new(exec.Cmd)
+	flag.Parse()
 
-			switch runtime.GOOS {
-
-			case "linux":
-				cmd = exec.Command("xdg-open", "https://google.com/#q="+s)
-			case "windows":
-				cmd = exec.Command("cmd", "/c", "start", "https://google.com/#q="+s)
-			case "darwin":
-				cmd = exec.Command("open", "https://google.com/#q="+s)
-			default:
-				cmd = exec.Command("xdg-open", "https://google.com/#q="+s)
-				//TODO: Add support for more Operating Systems
-			}
-
-			err := cmd.Start()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+	//If the number of arguments is zero, exit the main() function
+	if flag.NArg() == 0 {
+		println("give a search query, e.g. \"google hello world\" ")
+		return
 	}
-	app.Run(os.Args)
+
+	s := strings.Join(flag.Args(), "+") //Concatenates the args with '+'
+	println("let me google", s)
+
+	cmd := new(exec.Cmd) //Pointer to newly allocated exec.Cmd type
+
+	switch runtime.GOOS {
+
+	case "linux":
+		cmd = exec.Command("xdg-open", "https://google.com/#q="+s)
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", "https://google.com/#q="+s)
+	case "darwin":
+		cmd = exec.Command("open", "https://google.com/#q="+s)
+	default:
+		println("I don't know how to google it, on this OS.")
+		println("Open an issue at github.com/hariharan-uno/google")
+
+	}
+
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }

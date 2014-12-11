7dcd6ece9f1eca850a3b0080babc9e79ead42b31
Hari haran <hariharan.uno@gmail.com>
2014-12-11 12:28:02 +0530

    Clean up code, use fmt

--- google.go
+++ google.go
@@ -1,7 +1,12 @@
+// Copyright 2014 Hari haran. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/url"
 	"os/exec"
@@ -13,25 +18,24 @@ func main() {
 
 	flag.Parse()
 
-	//If the number of arguments is zero, exit the main() function
+	// If the number of arguments is zero, exit the main() function.
 	if flag.NArg() == 0 {
-		println("give a search query, e.g. \"google hello world\" ")
+		fmt.Println(`give a search query, e.g. "google hello world" `)
 		return
 	}
 
-	s := strings.Join(flag.Args(), "+") //Concatenates the args with '+'
-	println("let me google", s)
+	s := strings.Join(flag.Args(), "+") // concatenate the args with '+'
+	fmt.Println("let me google", s)
 
 	link, err := url.Parse("https://google.com/#q=" + s)
 	if err != nil {
-		println("Incorrect url")
+		fmt.Printf("url parsing error: %s\n", err)
 		return
 	}
 
-	cmd := new(exec.Cmd) //Pointer to newly allocated exec.Cmd type
+	cmd := new(exec.Cmd)
 
 	switch runtime.GOOS {
-
 	case "linux":
 		cmd = exec.Command("xdg-open", link.String())
 	case "windows":
@@ -39,9 +43,8 @@ func main() {
 	case "darwin":
 		cmd = exec.Command("open", link.String())
 	default:
-		println("I don't know how to google it, on this OS.")
-		println("Open an issue at github.com/hariharan-uno/google")
-
+		fmt.Println("I don't know how to open a browser on this OS.")
+		fmt.Println("Open an issue at https://github.com/hariharan-uno/google/issues")
 	}
 
 	if err := cmd.Start(); err != nil {
